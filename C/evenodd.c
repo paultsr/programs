da28845ba5ca4e2ad4acf012d6a41b5878f0b470
saurabhasnare <66157342+saurabhasnare@users.noreply.github.com>
2020-10-02 17:32:19 +0530

    updated even odd

--- C/evenodd.c
+++ C/evenodd.c
@@ -8,9 +8,10 @@ Date : 17/10/2012
 #include<stdio.h>
 main()
 { int a;
-        printf("enter a no.");
+        printf("enter a number :");
         scanf("%d",&a);
         (a%2==0)?printf("%d is even",a):printf("%d is odd",a);
+     return 0;
 }
 
 /*
